64fd17fe0416c2fe98b4db5f58169e328157ea94
Akbar Izdihar <akbarizdihar@gmail.com>
2024-06-21 17:25:05 +0700

    Last Fix

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ from awDb import get_db_data
 
 # Retrieve data from database
 count_customer = get_db_data("""
-    SELECT dg.City, COUNT(dc.CustomerKey    ) AS CustomerCount
+    SELECT dg.City, COUNT(dc.CustomerKey) AS CustomerCount
     FROM dimcustomer dc
     JOIN dimgeography dg ON dc.GeographyKey = dg.GeographyKey
     GROUP BY dg.City
 """)
 
-    st.header('Line Chart of Customer Count per City')
-    line_fig = px.line(count_customer, x='City', y='CustomerCount', title='Customer Count per City')
-    st.plotly_chart(line_fig)
+st.header('Line Chart of Customer Count per City')
+line_fig = px.line(count_customer, x='City', y='CustomerCount', title='Customer Count per City')
+st.plotly_chart(line_fig)
 
-    st.write("Line Chart ini berfungsi untuk menampilkan asal kota Customer.")
+st.write("Line Chart ini berfungsi untuk menampilkan asal kota Customer.")
