d9796810176e313406ced591cb7fb3c3440a2cf6
Akbar Izdihar <akbarizdihar@gmail.com>
2024-06-21 17:17:45 +0700

    Top10

--- app.py
+++ app.py
@@ -5,10 +5,18 @@ from awDb import get_db_data
 
 # Retrieve data from database
 count_customer = get_db_data("""
-    SELECT dg.City, COUNT(dc.CustomerKey) AS CustomerCount
+    SELECT dg.City, COUNT(dc.CustomerKey    ) AS CustomerCount
     FROM dimcustomer dc
     JOIN dimgeography dg ON dc.GeographyKey = dg.GeographyKey
     GROUP BY dg.City
+""")
+top_city = get_db_data("""
+SELECT dg.City, COUNT(dc.CustomerKey) AS CustomerCount
+    FROM dimcustomer dc
+    JOIN dimgeography dg ON dc.GeographyKey = dg.GeographyKey
+    GROUP BY dg.City
+    ORDER BY CustomerCount DESC
+    LIMIT 10
 """)
 
 # Check if data retrieval is successful (optional)
@@ -22,3 +30,9 @@ else:
 
     # Additional description
     st.write("This line chart shows the number of customers per city. It helps visualize the distribution of customers across different cities.")
+
+# Iterate over the top 10 cities and display them with a progress bar
+for index, row in customer_data_df.iterrows():
+    progress_percent = row['CustomerCount'] / customer_data_df['CustomerCount'].max()
+    st.write(f"{row['City']} - Total Customers: {row['CustomerCount']}")
+    st.progress(progress_percent)
