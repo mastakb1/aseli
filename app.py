3492c6e9975381df84d141c1a2f6ac8dbf42d3d7
Akbar Izdihar <akbarizdihar@gmail.com>
2024-06-21 16:54:24 +0700

    Try line chart

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from awDb import get_db_data
+
+count_customer = get_db_data("""
+SELECT dg.City, COUNT(dc.CustomerID) AS CustomerCount
+FROM dimcustomer dc
+JOIN dimgeography dg ON dc.GeographyKey = dg.GeographyKey
+GROUP BY dg.City;
+""")
+
+st.header('Line Chart of Customer Count per City')
+line_fig = px.line(customer_data_df, x='City', y='CustomerCount', title='Customer Count per City')
+st.plotly_chart(line_fig)
