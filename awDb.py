5138eddae31463c48a344684524083a588b0d869
Akbar Izdihar <akbarizdihar@gmail.com>
2024-06-21 17:10:13 +0700

    fixing, again

--- awDb.py
+++ awDb.py
@@ -4,17 +4,16 @@ from dbConfig import read_config
 
 def create_connection():
     config = read_config()
-    return mysql.connector.connect(
-        host=config['host'],
-        port=config['port'],
-        user=config['user'],
-        password=config['password'],
-        database=config['database']
-    )
-
-
-def get_db_data(query):
-    conn = create_connection()
-    df = pd.read_sql(query, conn)
-    conn.close()
-    return df
+    print(f"Config read: {config}")  # Check if config is correctly populated
+    try:
+        conn = mysql.connector.connect(
+            host=config['host'],
+            port=config['port'],
+            user=config['user'],
+            password=config['password'],
+            database=config['database']
+        )
+        return conn
+    except mysql.connector.Error as e:
+        print(f"Error connecting to MySQL: {e}")
+        return None
--- dbConfig.py
+++ dbConfig.py
@@ -1,25 +1,12 @@
-import streamlit as st
-import os
 import toml
+import os
 
-# Fungsi untuk membaca konfigurasi
 def read_config():
     config = {}
     
-    # Coba membaca dari file config.toml jika ada
     if os.path.exists('config.toml'):
         config = toml.load('config.toml')['database']
-    elif st.secrets:
-        # Jika tidak ada file config.toml, gunakan secrets Streamlit jika tersedia
-        config = {
-            'host': st.secrets["host"],
-            'port': st.secrets["port"],
-            'user': st.secrets["user"],
-            'password': st.secrets["password"],
-            'database': st.secrets["database"]
-        }
     else:
-        # Jika tidak ada file config.toml dan tidak ada secrets Streamlit, gunakan default
         config = {
             'host': os.getenv('DB_HOST', 'kubela.id'),
             'port': int(os.getenv('DB_PORT', '3306')),
